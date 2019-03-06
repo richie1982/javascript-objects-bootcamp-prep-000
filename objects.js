9f7edbf5ae635525929aba41453c4a1a240bad71
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-06 13:21:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var meals = { breakfast: "oatmeal" };
 var meals = new Object({
   breakfast: 'oatmeal'
 });
-var playlist = { kendrick: "high power", travis: "goosebumps" }
+var playlist = { kendrick: "high power", travis: "goosebumps" };
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName(songTitle);
 }
